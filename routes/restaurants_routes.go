0c54db333a75c1ded0f95939209413048abc4b85
Wan Heng Woon <hengwoon@boostorder.com>
2023-02-04 18:33:22 +0800

    add restaurants router

--- routes/restaurants_routes.go
+++ routes/restaurants_routes.go
@@ -8,4 +8,8 @@ import (
 
 func ResRoute(router *gin.Engine) {
 	router.POST("/Restaurants", controllers.CreateRes())
+	router.GET("/Restaurants/:id", controllers.GetRes())
+	router.GET("/Restaurants", controllers.GetAllRes())
+	router.DELETE("/Restaurants/:id", controllers.DeleteRes())
+	router.PUT("/Restaurants/:id", controllers.UpdateRes())
 }
