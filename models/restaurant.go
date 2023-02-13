963537cc8ea054dce943b9cea8e7e103a056e8b2
Wan Heng Woon <hengwoon@boostorder.com>
2023-02-13 21:56:33 +0800

    no update

--- models/restaurant.go
+++ models/restaurant.go
@@ -2,41 +2,49 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Restaurant struct {
-	Id            primitive.ObjectID `json:"id,omitempty"`
-	Name          string             `json:"name,omitempty"`
-	Type          string             `json:"type,omitempty"` //foodpanda,grabfood,googlemap,standalone
-	ContactNumber string             `json:"contact,omitempty"`
-	ServiceOption []ServiceOption    `json:"serviceoption,omitempty"` //dinein,takeaway
-	OpenHours     string             `json:"hours,omitempty"`
-	Website       string             `json:"website,omitempty"`
-	Address       string             `json:"address,omitempty"`
-	Rating        []Rating           `json:"rating,omitempty"`
-	Menu          []Menu             `json:"menu,omitempty"`
-}
-
-type Menu struct {
-	Id   primitive.ObjectID `json:"id,omitempty"`
-	Name string             `json:"name,omitempty"`
-	Code string             `json:"code"`
-	//pictures
+	Id            int             `json:"id,omitempty"`
+	Name          string          `json:"name,omitempty"`
+	Type          string          `json:"type,omitempty"` //foodpanda,grabfood,googlemap,standalone
+	ContactNumber string          `json:"contact,omitempty"`
+	ServiceOption []ServiceOption `json:"service_option,omitempty"` //dinein,takeaway
+	OpenHours     string          `json:"hours,omitempty"`
+	Website       string          `json:"website,omitempty"`
+	Address       string          `json:"address,omitempty"`
+	Status        string          `json:"status,omitempty"` //active, deleted,hided
+	Rating        []Rating        `json:"rating,omitempty"`
+	Menu          []MenuHeader    `json:"menu,omitempty"`
 }
 
 type Rating struct {
-	Id        primitive.ObjectID `json:"id,omitempty"`
-	UserId    uint8              `json:"userId,omitempty"`
-	CreatedAt time.Time          `json:"createdat,omitempty"`
-	UpdatedAt time.Time          `json:"updatedat,omitempty"`
-	Status    string             `json:"status,omitempty"` //active,deleted
-	Rating    float32            `json:"rating,omitempty"`
-	Comment   string             `json:"comment,omitempty"`
+	Id        int       `json:"id,omitempty"`
+	UserId    uint8     `json:"user_id,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	Status    string    `json:"status,omitempty"` //active,deleted,hided
+	Rating    float32   `json:"rating,omitempty"`
+	Comment   string    `json:"comment,omitempty"`
 }
 
 type ServiceOption struct {
-	Id         primitive.ObjectID `json:"id,omitempty"`
-	OptionName string             `json:"option,omitempty"`
+	OptionName string `json:"option,omitempty"`
+}
+
+type MenuHeader struct {
+	Id        int           `json:"id,omitempty"`
+	MenuKey   int           `json:"menu_key,omitempty"`
+	CreatedAt time.Time     `json:"created_at,omitempty"`
+	UpdatedAt time.Time     `json:"updated_at,omitempty"`
+	Menu      []MenuDetails `json:"menu_details,omitempty"`
+}
+
+type MenuDetails struct {
+	Id     int     `json:"id,omitempty"`
+	Name   string  `json:"name,omitempty"`
+	Code   string  `json:"code,omitempty"`
+	Images []byte  `json:"images,omitempty"`
+	Price  float32 `json:"price,omitempty"`
+	Status string  `json:"status,omitempty"`
 }
