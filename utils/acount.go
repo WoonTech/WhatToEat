f42cc70e909787f99ed7dcdf5d2b7da61086fef9
wan heng woon <wan_heng_woon@hotmail.com>
2023-02-19 11:28:06 +0800

    fixing auth

---
+++ utils/acount.go
@@ -0,0 +1,22 @@
+package utils
+
+/*func isValidEmail(email string) bool {
+	if email == "" {
+		return false
+	}
+	if strings.Contains(email, "|") {
+		return false
+	}
+	if email[len(email)-1] == '.' {
+		return false
+	}
+	return
+}*/
+
+/*func isValidContact(contact string) bool {
+
+}
+
+func isValidPassword(password string) bool {
+
+}*/
